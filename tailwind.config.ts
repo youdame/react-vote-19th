0a811ecba4d09b10ca4436a657d745977696d1e3
CSE-pebble <CSE.pebble@gmail.com>
2024-06-23 20:12:50 +0900

    Chore: pxr 단위 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,32 @@
 import type { Config } from "tailwindcss";
 
+type AccType = Record<string, string>;
+
+const range = (start: number, end: number): number[] => {
+  const array = [];
+  for (let i = start; i <= end; ++i) {
+    array.push(i);
+  }
+  return array;
+};
+
+const pxToRem = (px: number, base = 16) => `${px / base}rem`;
+
 const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
+    spacing: {
+      ...range(1, 2000).reduce((acc: AccType, px: number) => {
+        acc[`${px}pxr`] = pxToRem(px);
+        return acc;
+      }, {}),
+    },
+    fontSize: {
+      ...range(0, 2000).reduce((acc: AccType, px: number) => {
+        acc[`${px}pxr`] = pxToRem(px);
+        return acc;
+      }, {}),
+    },
     extend: {
       fontFamily: {
         pretendard: ["Pretendard", "sans-serif"],
