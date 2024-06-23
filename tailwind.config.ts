0b2934afbdb3ecee42d18540b3c5b46b21529665
youdame <112458620+youdame@users.noreply.github.com>
2024-06-23 18:18:34 +0900

    Style : 폰트 설정 완료

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,13 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      extend: {
+        fontFamily: {
+          pretendard: ["Pretendard", "sans-serif"],
+        },
+      },
+    },
   },
   plugins: [],
 };
