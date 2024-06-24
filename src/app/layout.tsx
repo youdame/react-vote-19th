ea41bb546785fe9f0451c4db67e0cb928c70fbd7
youdame <112458620+youdame@users.noreply.github.com>
2024-06-24 12:38:53 +0900

    Feat : 인풋창 만들기 #1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body className="flex min-h-screen justify-center">{children}</body>
     </html>
   );
 }
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,5 +1,14 @@
 function SignupPage() {
-  return <div>회원가입</div>;
+  return (
+    <div className="flex-center w-full bg-blue-base">
+      <div className="h-850pxr w-800pxr rounded-3xl bg-white px-60pxr py-30pxr">
+        <div className="flex-column gap-15pxr">
+          {/* <label className="text-20pxr text-black">이름</label>
+          <input className="w-660pxr h-60pxr px-15pxr py-18pxr focus:border-blue-base rounded-xl border border-gray-400"></input> */}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SignupPage;
