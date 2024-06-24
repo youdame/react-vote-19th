e4dfa29f73eda94716315e64f1ac3c925e53b17e
CSE-pebble <CSE.pebble@gmail.com>
2024-06-25 08:04:06 +0900

    Feat: 데모데이 투표 화면 퍼블리싱

--- src/app/demo-day/page.tsx
+++ src/app/demo-day/page.tsx
@@ -1,5 +1,35 @@
+import GenSelectBtn from "@/components/common/GenSelectBtn";
+import VoteTitle from "@/components/common/VoteTitle";
+import Link from "next/link";
+
+/* TODO : API 연결
+1. 임시 변수
+*/
+const isVoteDone = false; // 임시 변수
+
 function DemoDayPage() {
-  return <div>데모데이 투표</div>;
+  return (
+    <main className="flex-center flex-column">
+      <VoteTitle>데모데이 투표</VoteTitle>
+      <div className="flex-column gap-25pxr pt-100pxr">
+        {isVoteDone ? (
+          <>
+            <GenSelectBtn variant="disabled">투표하기</GenSelectBtn>
+            <Link href="/demo-day/result">
+              <GenSelectBtn>결과보기</GenSelectBtn>
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link href="/demo-day/vote">
+              <GenSelectBtn>투표하기</GenSelectBtn>
+            </Link>
+            <GenSelectBtn variant="disabled">결과보기</GenSelectBtn>
+          </>
+        )}
+      </div>
+    </main>
+  );
 }
 
 export default DemoDayPage;
