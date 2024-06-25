54e3fc0786a1b8671cd41d45f8515ad8797bcb73
youdame <112458620+youdame@users.noreply.github.com>
2024-06-25 19:41:41 +0900

    Http 해결 중..

--- next.config.mjs
+++ next.config.mjs
@@ -10,10 +10,10 @@ const nextConfig = {
   },
   async rewrites() {
     return [
-      // {
-      //   source: "/api/v1/:path*", // 모든 /api/v1/* 요청을 프록시
-      //   destination: "http://43.200.141.226/api/v1/:path*", // 해당 경로로 프록시
-      // },
+      {
+        source: "/login", // /login 요청을 프록시
+        destination: "http://43.200.141.226/login", // 실제 API 경로
+      },
       {
         source: "/:path*", // 모든 /api/v1/* 요청을 프록시
         destination: "http://43.200.141.226/:path*", // 해당 경로로 프록시
