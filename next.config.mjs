fcd88f901560b01b3e620cea62f3460240d30434
youdame <112458620+youdame@users.noreply.github.com>
2024-06-25 19:29:23 +0900

    프록시 구현 중

--- next.config.mjs
+++ next.config.mjs
@@ -10,10 +10,10 @@ const nextConfig = {
   },
   async rewrites() {
     return [
-      {
-        source: "/api/v1/:path*", // 모든 /api/v1/* 요청을 프록시
-        destination: "http://43.200.141.226/api/v1/:path*", // 해당 경로로 프록시
-      },
+      // {
+      //   source: "/api/v1/:path*", // 모든 /api/v1/* 요청을 프록시
+      //   destination: "http://43.200.141.226/api/v1/:path*", // 해당 경로로 프록시
+      // },
       {
         source: "/:path*", // 모든 /api/v1/* 요청을 프록시
         destination: "http://43.200.141.226/:path*", // 해당 경로로 프록시
--- src/api/auth.ts
+++ src/api/auth.ts
@@ -20,6 +20,6 @@ export interface PostSignInReq {
 
 export const postSignIn = async (userData: PostSignInReq) => {
   // 전체 응답 객체를 반환하도록 설정
-  const response = await loginInstance.post("/login", userData);
+  const response = await loginInstance.post("login", userData);
   return response;
 };
