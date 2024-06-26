b6c5c74985c262d12cabfa9600e61d1ba485d121
CSE-pebble <CSE.pebble@gmail.com>
2024-06-27 03:21:49 +0900

    Feat: 테스트 데이터 삭제 및 투표 결과 API 연결 완료

--- src/app/demo-day/result/page.tsx
+++ src/app/demo-day/result/page.tsx
@@ -1,33 +1,18 @@
+"use client";
+import { getTeamVoteResult } from "@/api/demo-day";
 import PageTemplate from "@/components/common/PageTemplate";
 import ResultPageTemplate from "@/components/common/ResultPageTemplate";
-
-const tempData = [
-  {
-    name: "Azito",
-    voteCount: 10,
-  },
-  {
-    name: "BeatBuddy",
-    voteCount: 9,
-  },
-  {
-    name: "CoupleLog",
-    voteCount: 7,
-  },
-  {
-    name: "PetPlate",
-    voteCount: 5,
-  },
-  {
-    name: "TIG",
-    voteCount: 4,
-  },
-];
+import { useQuery } from "@tanstack/react-query";
 
 function DemoDayResultPage() {
+  const { data: resultData } = useQuery({
+    queryKey: ["results"],
+    queryFn: () => getTeamVoteResult(),
+  });
+
   return (
     <PageTemplate voteTitle="데모데이 투표 결과">
-      <ResultPageTemplate data={tempData} variant="team" />
+      <ResultPageTemplate data={resultData ?? []} variant="team" />
     </PageTemplate>
   );
 }
