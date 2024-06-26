2859ff7da60c2d18114dc7f40266cb9d5a5833ae
CSE-pebble <CSE.pebble@gmail.com>
2024-06-27 03:34:12 +0900

    Edit: 유저 투표 여부에 따라 투표 버튼 disabled 되는 기능 삭제

--- src/app/demo-day/page.tsx
+++ src/app/demo-day/page.tsx
@@ -2,22 +2,13 @@ import Link from "next/link";
 import PageTemplate from "@/components/common/PageTemplate";
 import GenSelectBtn from "@/components/common/GenSelectBtn";
 
-/* TODO : API 연결
-1. 임시 변수
-*/
-const isVoteDone = false; // 임시 변수
-
 function DemoDayPage() {
   return (
     <PageTemplate voteTitle="데모데이 투표">
       <div className="flex-column gap-25pxr pt-100pxr">
-        {isVoteDone ? (
-          <GenSelectBtn variant="disabled">투표하기</GenSelectBtn>
-        ) : (
-          <Link href="/demo-day/vote">
-            <GenSelectBtn>투표하기</GenSelectBtn>
-          </Link>
-        )}
+        <Link href="/demo-day/vote">
+          <GenSelectBtn>투표하기</GenSelectBtn>
+        </Link>
         <Link href="/demo-day/result">
           <GenSelectBtn>결과보기</GenSelectBtn>
         </Link>
--- src/app/leader/page.tsx
+++ src/app/leader/page.tsx
@@ -3,44 +3,25 @@ import PageTemplate from "@/components/common/PageTemplate";
 import BigSelectBtn from "@/components/common/BigSelectBtn";
 import GenSelectBtn from "@/components/common/GenSelectBtn";
 
-/* TODO : API 연결
-1. 임시 변수
-*/
-const isVoteFEDone = false; // 임시 변수
-const isVoteBEDone = true; // 임시 변수
-
 function LeaderPage() {
   return (
     <PageTemplate voteTitle="파트장 투표">
       <div className="flex gap-100pxr pt-70pxr">
         <div className="flex-column flex-center gap-20pxr">
-          {isVoteFEDone ? (
-            <BigSelectBtn btnTexts={["FRONT-END", "파트장 투표"]} variant={"disabled"} />
-          ) : (
-            <Link href="/leader/vote-front">
-              <BigSelectBtn btnTexts={["FRONT-END", "파트장 투표"]} />
-            </Link>
-          )}
+          <Link href="/leader/vote-front">
+            <BigSelectBtn btnTexts={["FRONT-END", "파트장 투표"]} />
+          </Link>
           <Link href="/leader/result-front">
             <GenSelectBtn>결과보기</GenSelectBtn>
           </Link>
         </div>
         <div className="flex-column flex-center gap-20pxr">
-          {isVoteBEDone ? (
-            <>
-              <BigSelectBtn btnTexts={["BACK-END", "파트장 투표"]} variant="disabled" />
-              <Link href="/leader/result-back">
-                <GenSelectBtn>결과보기</GenSelectBtn>
-              </Link>
-            </>
-          ) : (
-            <>
-              <Link href="/leader/vote-back">
-                <BigSelectBtn btnTexts={["BACK-END", "파트장 투표"]} />
-              </Link>
-              <GenSelectBtn variant="disabled">결과보기</GenSelectBtn>
-            </>
-          )}
+          <Link href="/leader/vote-back">
+            <BigSelectBtn btnTexts={["BACK-END", "파트장 투표"]} />
+          </Link>
+          <Link href="/leader/result-back">
+            <GenSelectBtn>결과보기</GenSelectBtn>
+          </Link>
         </div>
       </div>
     </PageTemplate>
